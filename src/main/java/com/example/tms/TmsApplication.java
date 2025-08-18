1278874b2bf4944e3bbb1459149ff19f72dcf1b6
busyarone <busyarone@gmail.com>
2025-08-18 14:02:52 -0700

    Adding additional config files

--- src/main/java/com/example/tms/TmsApplication.java
+++ src/main/java/com/example/tms/TmsApplication.java
@@ -2,12 +2,14 @@ package com.example.tms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TmsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(TmsApplication.class, args);
+
 	}
 
 }
